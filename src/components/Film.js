a14b20ded3ed7aa0ce902edcae80c16dc41b4c08
klamb95 <klamb1995@gmail.com>
2021-05-10 16:24:40 +0100

    url displaying

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Film = ({name, url}) => {
+const Film = ({name, url, children}) => {
     return(
         <>
         <h4>{name}</h4>
         <h5>{url}</h5>
+        <p>{children}</p>
         </>
     )
 };
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -4,7 +4,7 @@ import Film from './Film';
 const FilmList = ({films}) => {
     const filmNodes = films.map(film =>{
         return(
-            <Film name={film.name} key={film.id}>{film.name}</Film>
+            <Film name={film.name} key={film.id}>{film.url}</Film>
         )
     })
 
